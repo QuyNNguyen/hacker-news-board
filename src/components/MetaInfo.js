5142d6a0e4e7d1c250cf0a13ade30cf481d5b0ef
Quy <ngocquy.nu4@gmail.com>
2021-09-29 16:04:53 +0700

    added unix date formmating fuction

--- src/components/MetaInfo.js
+++ src/components/MetaInfo.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+//Format unix time stamp to human readable format
+export function formatTime(time) {
+  const dateObject = new Date(time * 1000);
+  return dateObject.toLocaleString();
+}
+
 
 export default function MetaInfo(props) {
   const {by, time, comments} = props;
+  var formatedTime = formatTime(time)
   return (
-      <p className="meta-info-light">
-        by {by} on {time} with {comments} comments
-      </p>
+    <p className="meta-info-light">
+      by {by} on {formatedTime} with {comments} comments
+    </p>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,6 @@ import React from "react"
 
 export default function Title(props){
   const {text, url} = props
-  console.log(url)
   return(
     // <div className = "link">{props.text}</div>
     <a className = "link" href={url}>{text}</a>
