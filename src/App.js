6f5ac701d668c6752313ea2fc31d85c1c1bcd8eb
Quy <ngocquy.nu4@gmail.com>
2021-10-02 09:23:23 +0700

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Nav from "./components/Nav.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NewsBoard from "./components/NewsBoard";
+import { BoxLoading } from "react-loadingg";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <Router>
+        <div className="container">
+          <Nav />
+          <React.Suspense fallback={<BoxLoading color="rgb(187, 46, 31)" />}>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => <NewsBoard {...props} type={"top"} />}
+              />
+              <Route
+                exact
+                path="/new"
+                render={(props) => <NewsBoard {...props} type={"new"} />}
+              />
+            </Switch>
+          </React.Suspense>
+        </div>
+      </Router>
+    </React.StrictMode>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Nav from "./components/Nav.js";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NewsBoard from "./components/NewsBoard";
-import { BoxLoading } from "react-loadingg";
+import App from "./App";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <div className="container">
-        <Nav />
-        <React.Suspense fallback={<BoxLoading color="rgb(187, 46, 31)" />}>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => <NewsBoard {...props} type={"top"} />}
-            />
-            <Route
-              exact
-              path="/new"
-              render={(props) => <NewsBoard {...props} type={"new"} />}
-            />
-          </Switch>
-        </React.Suspense>
-      </div>
-    </Router>
-  </React.StrictMode>,
+  <App/>,
   document.getElementById("root")
 );
 
